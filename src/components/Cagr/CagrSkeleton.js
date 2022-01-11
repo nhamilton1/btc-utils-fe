5cede07c9f5f733ba560c80955b2101f813ad113
nhamilton1 <nickh3592@gmail.com>
2022-01-11 08:02:20 -0500

    styling for skeleton loading for chart

--- src/components/Cagr/CagrSkeleton.js
+++ src/components/Cagr/CagrSkeleton.js
@@ -1,11 +1,29 @@
-import { Skeleton } from "antd";
+import { Col, Row, Skeleton } from "antd";
+import Layout from "antd/lib/layout/layout";
 import React from "react";
 
 const CagrSkeleton = () => {
   return (
-    <div style={{ width: "50%" }}>
-      <Skeleton active paragraph={{ rows: 4 }} />
-    </div>
+    <Layout >
+      <Row>
+        <Col span={19}>
+          <Skeleton active paragraph={{ rows: 10 }} />
+        </Col>
+        <Col
+          span={5}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexFlow: "column wrap",
+          }}
+        >
+          <Skeleton.Button size="large" style={{margin: '1vh'}}/>
+          <Skeleton.Button size="large" style={{margin: '1vh'}}/>
+          <Skeleton.Button size="large" style={{margin: '1vh'}}/>
+        </Col>
+      </Row>
+    </Layout>
   );
 };
 
